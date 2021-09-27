bc5f7e29c3cdcc17f67cfcf5e3814b9af7c4a1e6
Thea Vanherst <45670606+TheaVanherst@users.noreply.github.com>
2021-09-27 06:26:41 +0100

    Add files via upload

--- pagescripts.js
+++ pagescripts.js
@@ -9,12 +9,39 @@ $(document).ready(resize); $(window).on("load resize", resize)
 
 $(document).ready(function() {
     if (/Mobi|Android/i.test(navigator.userAgent) === true) {
-        $(".nmb, #top-nav, #side").addClass("active");
+        $(".nmb, #top-nav, #side, .pg0 siders").addClass("active");
         $(".pg").removeClass("active")}
 
     $('#pfp').attr('src',"/Website Assets/imgs/pfp.gif");
     $('#sc1').attr('src',"/Website Assets/imgs/ams1.png")
 })
+
+var arthover = function () {
+    $('incont > *').children('div.zoom').css("opacity","1")
+    $('#hover-element').remove(); $('#side').css("z-index","99")
+
+    var target = $('#' + event.target.id);
+    target.clone(true, true).insertAfter(target);
+    $(target).attr("id", "hover-element");
+
+    var image, image_url = $('#hover-element').css('background-image');
+    if (image_url !== undefined) {
+        image_url = image_url.match(/^url\("?(.+?)"?\)$/);
+        image_url = image_url[1];
+        image = new Image();
+        image.src = image_url;
+
+        $('#hover-element').css({
+            "width": target.height() * (image.width / image.height),
+            "height": target.height(), "min-width":target.parent().width()}).css(
+            "margin-left", ((target.width() / -2) + (target.parent().width() / 2))
+        ).parent().children('div.zoom').css("opacity","0.4")
+    }
+}
+
+$('incont > * > div').click(arthover).hover(arthover)
+$('incont > * > div').hover(function(){$('#side').css("z-index","0")
+}).mouseout(function() {$('#side').css("z-index","99")})
 
 $(function (){$('.hdrlogo').hover(
         function(){$('#hdr, #bck').addClass("hov")},
@@ -45,7 +72,7 @@ function loadpostelement(){
         if(days<=7){newpost = '<div class="new-post"></div>';}
 
         if(mnths<3){if(days<7){if(days==0){t+="earlier today at ";}else{t+="on "+day1+" at ";}t+=hours[0]+":"+hours[1]+pmam;}
-        else{if(week==1){t+="a week ago on"+day1;}else if(day>7&&day<14){t+="on "+day1+" last week at ";}else{t+=week+" weeks";}}}
+        else{if(week==1){t+="last week on "+day1+" the "+day2+nth;}else if(day>7&&day<14){t+="on "+day1+" last week at ";}else{t+=week+" weeks";}}}
         else if(yrs<1){t+=mnths+" months";}else{if(yrs==1){t+="a";}else{t+=yrs;}t+=" year s";}if(days>13){t+=" ago";}
 
         $('<article id=article'+article+'>'+
@@ -68,27 +95,29 @@ function loadpostelement(){
     })
 }
 
+var frt = ["jpg","png","gif"];
+
 var buttontext = ["Art Feed","Blog","Artwork","Design","Animation","Workshop","Javascript"],
     buttoncolour = ["#1da1f2","#5265fa","#171a21","#ff0000","#ff5e5b","#738adb"],
     tv = "/TheaVanherst", buttonlinks = ["twitter.com"+tv,"discord.com/invite/zDJRaZvGmS","steamcommunity.com/id"+tv,
         "youtube.com/channel/UCqbZe2XMkd98nXu_dUlObKA","ko-fi.com"+tv,"github.com"+tv]
 
 $(document).ready(function(){
-
-    for(var i=0; i<buttoncolour.length; i++){
+    for(var i = 0; i<buttoncolour.length; i++){
         $('<a target="_blank" href="https://'+buttonlinks[i]+'"><div class="wdt i'+(i)+'"></div></a>').appendTo("#side-media");
         $('head').append('<style>#side-media:hover > a:nth-child('+(i+1)+'):hover{background-color:'+buttoncolour[i]+'}</style>');}
 
-    for(var i=0; i<buttontext.length; i++){
+    for(var i = 0; i<buttontext.length; i++){
         var temp = '#side-nav';
         if(i != 5){temp = temp + ', #top-nav'}
-        $('<div class="ls pg'+i+'"><t>'+buttontext[i]+'</t><div class="i bt'+i+'"></div></div>').appendTo(temp);}})
+        $('<div class="ls pg'+i+'"><t>'+buttontext[i]+'</t><div class="i bt'+i+'"></div></div>').appendTo(temp)}
+})
 
 var pg2l=[1,1,1,1,2,1,1,1],pg2m=[1,1,1,1,1,1,1,1],pg2r=[1,1,1,1,1,1,1,2],sid2a=[pg2l,pg2m,pg2r], //image format : frt[]
     pg2hl=[9,5,5,0,5,8,7,7],pg2hm=[8.5,8,8,8,3,4,5,8],pg2hr=[9,7,5,7,5,5,9,4],sid2h=[pg2hl,pg2hm,pg2hr], //image hights
-    frt=["jpg","png","gif"],sides = ["l","m","r"]; //for different file types, then loops through which coloumn is currently being looped
+    sides = ["l","m","r"]; //for different file types, then loops through which coloumn is currently being looped
 
-var pgdh=[460,460,460,475,475,234,610,610,610,610,610,610,234,475,475,450,450,234,234,595,595,595,153,206,450],
+var pgdh=[430,430,430,465,465,224,600,600,600,600,600,600,224,465,465,440,440,224,224,575,575,575,143,196,440],
     pg3h=[355,355,355,425,425,184,560,560,560,560,772,560,184,425,425,400,400,184,184,460,460,460,153,156,400], //image hights
     ap3=[0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1]
 
